a18519cf7e52df02a8e4c58eac5c0202fba77924
Supriya-Kumari13 <137148773+Supriya-Kumari13@users.noreply.github.com>
2025-03-30 22:35:10 +0530

    Add files via upload

---
+++ Problem1.py
@@ -0,0 +1,10 @@
+# Printing the "Twinkle, Twinkle, Little Star" poem using a multi-line string
+
+print("""
+Twinkle, twinkle, little star,
+    How I wonder what you are!
+        Up above the world so high,
+        Like a diamond in the sky.
+Twinkle, twinkle, little star,
+    How I wonder what you are!
+""")
