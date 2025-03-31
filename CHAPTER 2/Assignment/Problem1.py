05306ea56465b61e875be833697d41b0f3f43423
Supriya-Kumari13 <137148773+Supriya-Kumari13@users.noreply.github.com>
2025-03-31 16:22:20 +0530

    adding assinment of chpter 2

---
+++ CHAPTER 2/Assignment/Problem1.py
@@ -0,0 +1,5 @@
+#write a program in python to add two numbers
+a = 5
+b= 10
+
+print("The sum of a and b is:", a + b)  # Output: 15
---
+++ CHAPTER 2/Assignment/Problem2.py
@@ -0,0 +1,6 @@
+# Write a python program to find a remainder when  a number is divided by z.
+# Take input from user
+a= int(input("Enter a number: "))
+z= int(input("Enter another number: "))
+
+print("The remainder when a is divided by z is:", a % z)  # Output: Remainder
