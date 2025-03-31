4527a7bd4345164ac7434c0c426ce636dc621b7f
Supriya-Kumari13 <137148773+Supriya-Kumari13@users.noreply.github.com>
2025-03-31 16:03:05 +0530

    adding input ralated file in chapter 2 folder

---
+++ CHAPTER 2/06_input.py
@@ -0,0 +1,8 @@
+"""
+This code demonstrates the use of input() function in Python.
+The input() function is used to take user input in Python.
+"""
+a = input("Enter a number: ") # Input is always a string
+b = input("Enter another number: ")
+print(a + b)  # Concatenate strings
+print(int(a) + int(b))  # Convert to integers and add
