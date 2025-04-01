6aedb529a14c783559c19e4c62e4893dcaefe1a6
Supriya-Kumari13 <137148773+Supriya-Kumari13@users.noreply.github.com>
2025-04-02 00:14:05 +0530

    adding new problem

---
+++ CHAPTER 2/Assignment/Problem6.py
@@ -0,0 +1,5 @@
+# write a program to calculate the saure of a number entered by user
+# take input from usera
+a = int(input("Enter a number: "))
+# calculate the square of a number
+print("The sqaure of given number", a ** 2)
