a8d14c9845a9ac86c7d9d35f76f51afe1c8193eb
Supriya-Kumari13 <137148773+Supriya-Kumari13@users.noreply.github.com>
2025-04-03 13:56:01 +0530

    adding chapter 3

---
+++ CHAPTER 3/02_String_Function.py
@@ -0,0 +1,11 @@
+Name = "Supriya"
+
+print(len(Name))
+print(Name.endswith("ya")) # it will check whether the string ends with "ya" or not
+print(Name.startswith("Su")) # it will check whether the string starts with "Su" or not
+print(Name.find("pr")) # it will find the index of "pr" in the string
+print(Name.count("a")) # it will count the number of "a" in the string
+print(Name.upper()) # it will convert the string to upper case
+print(Name.lower()) # it will convert the string to lower case
+print(Name.replace("Supriya", "Priya")) # it will replace "Supriya" with "Priya"
+print(Name.strip()) # it will remove any leading and trailing whitespaces
