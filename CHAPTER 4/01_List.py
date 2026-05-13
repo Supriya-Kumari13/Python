1345384ba91799dfa48bc351a153987e069c74e6
Supriya-Kumari13 <137148773+Supriya-Kumari13@users.noreply.github.com>
2026-05-13 10:01:29 +0530

    Update comments and indexing in 01_List.py

--- CHAPTER 4/01_List.py
+++ CHAPTER 4/01_List.py
@@ -1,7 +1,7 @@
-friends= ["Mahi", 5, 9.76, True, "Anjali"]  # List of friends
+friends= ["Mahi", 5, 9.76, True, "Anjali"]  # List of friends (indexing starts with zero)
 
-print(friends[4])  # Print the list of friends
+print(friends[3])  # Print the list of friends ouput-True
 
-friends[4] = False # unlike strings lists are mutable we can change the element
+friends[3] = False # unlike strings lists are MUTABLE we can change the element
 
-print(friends[4])
+print(friends[3])  #output False
